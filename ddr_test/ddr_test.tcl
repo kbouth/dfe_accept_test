b70e66f990b55242c56af126b45e57a72944ecaa
kbouth <kbouthsar1@bnl.gov>
2025-12-18 15:52:44 -0500

    updated ddr_test.tcl script; no longer need to edit paths

--- ddr_test/ddr_test.tcl
+++ ddr_test/ddr_test.tcl
@@ -1,17 +1,37 @@
+# Absolute path to this script's directory
+set SCRIPT_DIR [file dirname [file normalize [info script]]]
+
+puts "Running from: $SCRIPT_DIR"
+
 connect -url tcp:10.0.142.173:3121
-targets -set -nocase -filter {name =~"APU*"}
+
+targets -set -nocase -filter {name =~ "APU*"}
 rst -system
 after 3000
-targets -set -nocase -filter {name =~"APU*"}
-loadhw -hw /home/bouthsarath/dfe_accept_test/ddr_test/ddr_test.xsa -mem-ranges [list {0x80000000 0xbfffffff} {0x400000000 0x5ffffffff} {0x1000000000 0x7fffffffff}] -regs
+
+targets -set -nocase -filter {name =~ "APU*"}
+loadhw \
+  -hw [file join $SCRIPT_DIR ddr_test.xsa] \
+  -mem-ranges [list \
+    {0x80000000 0xbfffffff} \
+    {0x400000000 0x5ffffffff} \
+    {0x1000000000 0x7fffffffff}] \
+  -regs
+
 configparams force-mem-access 1
-targets -set -nocase -filter {name =~"APU*"}
-source /home/bouthsarath/dfe_accept_test/ddr_test/psu_init.tcl
+
+targets -set -nocase -filter {name =~ "APU*"}
+source [file join $SCRIPT_DIR psu_init.tcl]
 psu_init
 catch {psu_protection}
+
 targets -set -nocase -filter {name =~ "*A53*#0"}
 rst -processor
-dow /home/bouthsarath/dfe_accept_test/ddr_test/ddr_test.elf
+
+dow [file join $SCRIPT_DIR ddr_test.elf]
+
 configparams force-mem-access 0
+
 targets -set -nocase -filter {name =~ "*A53*#0"}
 con
+
